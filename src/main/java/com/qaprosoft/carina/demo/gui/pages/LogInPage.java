62a5e2bfd6247252ea3cb0997af983374c75e0d4
Vasil Laba <vasillaba1@gmail.com>
2023-02-24 17:32:48 +0200

    Add logIn page

---
+++ src/main/java/com/qaprosoft/carina/demo/gui/pages/LogInPage.java
@@ -0,0 +1,10 @@
+package com.qaprosoft.carina.demo.gui.pages;
+
+import com.qaprosoft.carina.core.gui.AbstractPage;
+import org.openqa.selenium.WebDriver;
+
+public class LogInPage extends AbstractPage {
+    public LogInPage(WebDriver driver) {
+        super(driver);
+    }
+}
