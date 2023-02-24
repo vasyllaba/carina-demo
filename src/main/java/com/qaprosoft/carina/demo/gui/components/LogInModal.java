db930708a59b0cc9141360b3de376c20c7bf76dc
Vasil Laba <vasillaba1@gmail.com>
2023-02-24 16:58:08 +0200

    add logIn modal class

---
+++ src/main/java/com/qaprosoft/carina/demo/gui/components/LogInModal.java
@@ -0,0 +1,11 @@
+package com.qaprosoft.carina.demo.gui.components;
+
+import com.qaprosoft.carina.core.gui.AbstractUIObject;
+import org.openqa.selenium.WebDriver;
+
+public class LogInModal extends AbstractUIObject {
+
+    public LogInModal(WebDriver driver) {
+        super(driver);
+    }
+}
