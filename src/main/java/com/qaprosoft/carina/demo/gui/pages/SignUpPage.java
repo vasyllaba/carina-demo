ea60fc8ba2571abf60b6fccf4ec2cb79490c658a
Vasil Laba <vasillaba1@gmail.com>
2023-02-24 18:16:07 +0200

    delete unusable imports

--- src/main/java/com/qaprosoft/carina/demo/gui/pages/SignUpPage.java
+++ src/main/java/com/qaprosoft/carina/demo/gui/pages/SignUpPage.java
@@ -2,7 +2,6 @@ package com.qaprosoft.carina.demo.gui.pages;
 
 import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebElement;
 import com.qaprosoft.carina.core.gui.AbstractPage;
-import com.qaprosoft.carina.demo.gui.components.LogInModal;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.FindBy;
 
