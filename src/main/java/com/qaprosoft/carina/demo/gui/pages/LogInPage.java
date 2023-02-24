f9ebed6d3f7a05db5d151f4687c881fb7df24284
Vasil Laba <vasillaba1@gmail.com>
2023-02-24 17:46:32 +0200

    add title and isPresent method to LogInPage

--- src/main/java/com/qaprosoft/carina/demo/gui/pages/LogInPage.java
+++ src/main/java/com/qaprosoft/carina/demo/gui/pages/LogInPage.java
@@ -1,10 +1,20 @@
 package com.qaprosoft.carina.demo.gui.pages;
 
+import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebElement;
 import com.qaprosoft.carina.core.gui.AbstractPage;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
 
 public class LogInPage extends AbstractPage {
+
+    @FindBy(xpath = "//div[@class='article-hgroup']/h1[@class='article-info-name']")
+    private ExtendedWebElement loginResult;
+
     public LogInPage(WebDriver driver) {
         super(driver);
+    }
+
+    public boolean isPresent() {
+        return loginResult.isPresent();
     }
 }
