ec5eb9f6cd7b52479e1b14efa511b89c3da31291
Vasil Laba <vasillaba1@gmail.com>
2023-02-28 16:54:38 +0200

    Create PageHendler class to switch and close pages

---
+++ src/main/java/com/qaprosoft/carina/demo/utils/PageHandler.java
@@ -0,0 +1,42 @@
+package com.qaprosoft.carina.demo.utils;
+
+import org.openqa.selenium.WebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.invoke.MethodHandles;
+
+
+public class PageHandler {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    /**
+     * Open any other page if present
+     *
+     * @param driver
+     */
+    public static void switchBetweenPages(WebDriver driver) {
+        LOGGER.info("Switch page");
+        if ((long) driver.getWindowHandles().size() == 1) {
+            openAvailablePage(driver);
+            return;
+        }
+
+        for (String windowHandle : driver.getWindowHandles()) {
+            if (!driver.getWindowHandle().contentEquals(windowHandle)) {
+                driver.switchTo().window(windowHandle);
+                break;
+            }
+        }
+    }
+
+    public static void closeCurrentPage(WebDriver driver) {
+        LOGGER.info("Close current page");
+        driver.close();
+    }
+
+    private static void openAvailablePage(WebDriver driver) {
+        LOGGER.info("Open available page");
+        driver.switchTo().window(driver.getWindowHandles().toArray()[0].toString());
+    }
+}
