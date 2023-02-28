20346c1723455dba1d082e8114c558a4e1c287f8
Vasil Laba <vasillaba1@gmail.com>
2023-02-28 16:53:31 +0200

    create project constatns class

---
+++ src/main/java/com/qaprosoft/carina/demo/projectConstants/ProjectConstants.java
@@ -0,0 +1,15 @@
+package com.qaprosoft.carina.demo.projectConstants;
+
+public class ProjectConstants {
+    public static final String NEWS_PAGE_URL = "https://www.gsmarena.com/news.php3";
+    public static final String REVIEWS_PAGE_URL = "https://www.gsmarena.com/reviews.php3";
+    public static final String VIDEOS_PAGE_URL = "https://www.gsmarena.com/videos.php3";
+    public static final String FEATURED_PAGE_URL = "https://www.gsmarena.com/news.php3?sTag=Featured";
+    public static final String PHONE_FINDER_PAGE_URL = "https://www.gsmarena.com/search.php3?";
+    public static final String DEALS_PAGE_URL = "https://www.gsmarena.com/deals.php3";
+    public static final String MERCH_PAGE_URL = "https://merch.gsmarena.com/";
+    public static final String COVERAGE_PAGE_URL = "https://www.gsmarena.com/network-bands.php3";
+    public static final String CONTACT_PAGE_URL = "https://www.gsmarena.com/contact.php3";
+    public static final String SIGN_UP_PAGE_URL = "https://www.gsmarena.com/register.php3";
+
+}
