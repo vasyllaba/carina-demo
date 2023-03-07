3a75148730ad4916c1fe8638018c8d9cf24652c1
Vasil Laba <vasillaba1@gmail.com>
2023-03-07 12:32:13 +0200

    Add project constants

--- src/main/java/com/qaprosoft/carina/demo/projectConstants/ProjectConstants.java
+++ src/main/java/com/qaprosoft/carina/demo/projectConstants/ProjectConstants.java
@@ -3,6 +3,7 @@ package com.qaprosoft.carina.demo.projectConstants;
 public class ProjectConstants {
     public static final String NEWS_PAGE_URL = "https://www.gsmarena.com/news.php3";
     public static final String REVIEWS_PAGE_URL = "https://www.gsmarena.com/reviews.php3";
+    public static final String COMPARE_PAGE_URL = "https://www.gsmarena.com/compare.php3";
     public static final String VIDEOS_PAGE_URL = "https://www.gsmarena.com/videos.php3";
     public static final String FEATURED_PAGE_URL = "https://www.gsmarena.com/news.php3?sTag=Featured";
     public static final String PHONE_FINDER_PAGE_URL = "https://www.gsmarena.com/search.php3?";
@@ -11,5 +12,18 @@ public class ProjectConstants {
     public static final String COVERAGE_PAGE_URL = "https://www.gsmarena.com/network-bands.php3";
     public static final String CONTACT_PAGE_URL = "https://www.gsmarena.com/contact.php3";
     public static final String SIGN_UP_PAGE_URL = "https://www.gsmarena.com/register.php3";
+    public static final String GLOSSARY_PAGE_URL = "https://www.gsmarena.com/glossary.php3";
+    public static final String FAQ_PAGE_URL = "https://www.gsmarena.com/faq.php3";
+    public static final String RSS_FEED_PAGE_URL = "https://www.gsmarena.com/rss-news-reviews.php3";
+    public static final String YOUTUBE_PAGE_URL = "https://www.youtube.com/channel/UCbLq9tsbo8peV22VxbDAfXA";
+    public static final String FACEBOOK_PAGE_URL = "https://www.facebook.com/people/GSMArenacom/100066846038410/";
+    public static final String TWITTER_PAGE_URL = "https://twitter.com/gsmarena_com";
+    public static final String INSTAGRAM_PAGE_URL = "https://www.instagram.com/gsmarenateam/";
+    public static final String TEAM_PAGE_URL = "https://www.gsmarena.com/team.php3";
+    public static final String HOME_PAGE_MOBILE_VERSION_URL = "https://m.gsmarena.com/";
+    public static final String ANDROID_APP_URL = "https://play.google.com/store/apps/details?id=com.gsmarena.android";
+    public static final String TOOLS_PAGE_URL = "https://www.gsmarena.com/tools.php3";
+    public static final String PRIVACY_PAGE_URL = "https://www.gsmarena.com/privacy-policy.php3";
+    public static final String TERM_OF_USE_PAGE_URL = "https://www.gsmarena.com/terms.php3";
 
 }
